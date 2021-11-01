99bfb6f673fb1a7bd9e07af972378b388ed476d6
혀느현스 <hyuns@hyuns.dev>
2021-11-02 01:07:14 +0900

    .

--- day1/1/sketch.js
+++ day1/1/sketch.js
@@ -2,16 +2,13 @@ function setup() {
     createCanvas(500, 500);
     background(255);
     smooth();
+    noStroke();
 }
 
 function draw() {
-    if (mousePressed) {
-        fill(0);
-    } else {
-        fill(255);
-    }
     // ellipse(mouseX, mouseY, 80, 80);
     // point(mouseX, mouseY, 250);
     // line(mouseX, mouseY, mouseX+10, mouseY+10);
+    console.log(frameCount);
     rect(100, 200, 100, 100);
 }
--- template/sketch.js
+++ template/sketch.js
@@ -1,8 +1,16 @@
 function setup() {
-    createCanvas(400, 200);
-    background(0);
+    createCanvas(800, 800);
+    background(255);
+    strokeWeight(4);
+    smooth();
+    noStroke();
+    stroke(0, 102);
 }
 
 function draw() {
-
+    // background(0);
+    // ellipse(mouseX, mouseY, 9, 9);
+    let weight = dist(mouseX, mouseY, pmouseX, pmouseY);
+    strokeWeight(weight);
+    line(mouseX, mouseY, pmouseX, pmouseY);
 }
